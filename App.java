ac7a991bed6a96cc0ace5967e569388290db3bbd
Alena Golovina and Sowmya Logaswamy <sowmya@>
2017-06-27 09:28:54 -0700

    Create Triangle-Tracker with a custom method, and a constructor

--- App.java
+++ App.java
@@ -4,11 +4,16 @@ public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
 
-    System.out.println("Enter the 3 lengths of the triangle.");
+    System.out.println("Enter the 1st length of the triangle.");
     int length1 = Integer.parseInt(myConsole.readLine());
+    System.out.println("Enter the 2nd length of the triangle.");
     int length2 = Integer.parseInt(myConsole.readLine());
+    System.out.println("Enter the 3rd length of the triangle.");
     int length3 = Integer.parseInt(myConsole.readLine());
 
     Triangle newTriangle = new Triangle(length1, length2, length3);
+     String result = newTriangle.determineTriangle(length1, length2, length3);
+     System.out.println(result);
+     
   }
 }
--- Triangle.java
+++ Triangle.java
@@ -3,10 +3,25 @@ class Triangle {
   public int mSide2;
   public int mSide3;
 
-  public String determineTriangle(int side1, int side2, int side3) {
-    if ((side1 == side2) && (side2 == side3)) {
-      return "The triangle is Equilateral";
-  }
-  }
-
+public Triangle(int side1, int side2, int side3) {
+  mSide1 = side1;
+  mSide2 = side2;
+  mSide3 = side3;
 }
+
+
+  public String determineTriangle(int side1, int side2, int side3) {
+    if(side1 == 0 || side2 == 0 || side3 == 0) {
+      return "Enter valid lengths";
+    } else if(((side1 + side2) < side3) || ((side2 + side3) < side1) || ((side3 + side1) < side2)) {
+      return " It is not a triangle";
+    } else if ((side1 == side2) && (side2 == side3)) {
+      return "The triangle is Equilateral";
+    } else if ((side1 == side2) || (side2 == side3) || (side1 == side3)) {
+      return "The triangle is Isosceles.";
+    } else if ((side1 != side2) && (side2 != side3) && (side3 != side1)) {
+      return "The triangle is Scalene.";
+    }
+    return ("possible error.");
+    }
+  }
